b505bf06b0297962b2567d01ea1f4c47026d1b39
FlashCat <FlashCat@users.noreply.github.com>
2015-03-26 18:55:20 +0000

    rebuild pages at ba2b993

--- implementors/core/ops/trait.Drop.js
+++ implementors/core/ops/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['linked-hash-map'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;K, V, S&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='linked-hash-map/struct.LinkedHashMap.html' title='linked-hash-map::LinkedHashMap'>LinkedHashMap</a>&lt;K, V, S&gt;",];
+implementors['linked_hash_map'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;K, V, S&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='linked_hash_map/struct.LinkedHashMap.html' title='linked_hash_map::LinkedHashMap'>LinkedHashMap</a>&lt;K, V, S&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
